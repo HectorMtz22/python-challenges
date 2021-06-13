13adeba4da54cd12a19bd5b7f061a5da09a47b4e
HectorMtz22 <hec.gre81@gmail.com>
2021-06-13 18:03:39 -0500

    Try catch and user input

--- src/4_pi.py
+++ src/4_pi.py
@@ -1,11 +1,16 @@
-def calculate_pi(number):
+def calculate_pi(n):
     sum = 0
-    for i in range(0, number):
+    for i in range(0, n):
         formula = ((4/(8 * i + 1)) - (2/(8 * i + 4)) - (1/(8 * i + 5)) - (1/(8 * i + 6)))  * ((1/16)**i)
         sum += formula
     
     return sum
 
 
-total = calculate_pi(10)
-print(total)
+# Init of program
+cifra_pi = input("Introduce una cifra para calcular pi con precisión\n")
+try:
+    total = calculate_pi(int(cifra_pi)) # Call the function
+    print("Pi is", total) # Print results
+except ValueError:
+    print("Invalid entries!") # If sth wents wrong
